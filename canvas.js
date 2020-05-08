6d7f054ed5e66bc6d243dcaa7a492857410d82f0
alexander-braun <alexanderbraun@freenet.de>
2020-05-08 19:43:01 +0200

    done with exchangable colors and radius sizes

--- canvas.js
+++ canvas.js
@@ -4,7 +4,7 @@ canvas.height = window.innerHeight
 
 const c = canvas.getContext('2d')
 
-c.fillStyle = 'red'
+
 
 /*
 c.fillRect(200, 200, 200, 200)
@@ -107,12 +107,26 @@ let mouse = {
     y: undefined
 }
 
-const maxRadius = 40
+const maxRadius = 50
 const minRadius = 0
+
+const colorArr = [
+    '#D9961A',
+    '#BF6B04',
+    '#A64F03',
+    '#731702',
+    '#260101'
+]
 
 window.addEventListener('mousemove', (e) => {
     mouse.x = event.x
     mouse.y = event.y
+})
+
+window.addEventListener('resize', (e) => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    init()
 })
 
 class Circle {
@@ -122,12 +136,13 @@ class Circle {
         this.dx = dx
         this.dy = dy
         this.radius = radius
+        this.minRadius = minRadius
+        this.color = colorArr[Math.floor(Math.random() * colorArr.length)]
 
         this.draw = () => {
             c.beginPath()
             c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-            c.strokeStyle = 'transparent'
-            c.stroke()
+            c.fillStyle = this.color
             c.fill()
         }
 
@@ -138,6 +153,7 @@ class Circle {
             if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
                 this.dy = -this.dy
             }
+
             this.x += this.dx
             this.y += -this.dy
 
@@ -146,7 +162,7 @@ class Circle {
                 if(this.radius < maxRadius) {
                     this.radius += 1    
                 }
-            } else if(this.radius > minRadius) {
+            } else if(this.radius > this.minRadius) {
                     this.radius -= 1
             }
 
@@ -156,15 +172,19 @@ class Circle {
 }
 
 let circleArr = []
-for(let i = 0; i < 300; i++) {
-    let radius = 3
-    let x = Math.random() * (innerWidth - radius * 2) + radius
-    let y = Math.random() * (innerHeight -radius * 2) + radius
-    let dx = (Math.random() - 0.5) * 10
-    let dy = (Math.random() - 0.5) * 10   
-    circleArr.push(new Circle(x, y, dx, dy, radius))
+function init() {
+    circleArr = []
+    for(let i = 0; i < 800; i++) {
+        let radius = Math.ceil(Math.random() * 15 + 1)
+        let x = Math.random() * (innerWidth - radius * 2) + radius
+        let y = Math.random() * (innerHeight -radius * 2) + radius
+        let dx = (Math.random() - 0.5) * 10
+        let dy = (Math.random() - 0.5) * 10   
+        circleArr.push(new Circle(x, y, dx, dy, radius))
+    }
 }
 
+init()
 
 const animate = () => {
     requestAnimationFrame(animate)
